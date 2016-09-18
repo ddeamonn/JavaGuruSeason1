4f0c7858d0e66669d19a4cc3892ce02217a35e44
ddeamonn <troo@inbox.lv>
2016-09-18 18:11:52 +0300

    Some methods refactored

--- src/webshop/Ui.java
+++ src/webshop/Ui.java
@@ -5,17 +5,54 @@ import java.util.*;
 /**
  * Created by Amir on 06.09.2016..
  */
-public class Ui {
 
-    public static void showMessage(String message){
-        System.out.println(message);
-    }
+public class Ui implements ClientUI, AdminUI {
 
-    public static void showBasketMenu(User user) {
+    public void userLogin(User user) {
 
     }
 
-    public static void showBasketContent(User user, List productBasket) {
+    public void userLogout(User user) {
+
+    }
+
+    public void addProductToBasket(Product product, UserBasket basket) {
+
+    }
+
+    public void showProductsInBasket(UserBasket basket) {
+
+    }
+
+    public void removeProductFromBasket(Product product, UserBasket basket) {
+
+    }
+
+    public void buyProductsInBasket(UserBasket basket) {
+
+    }
+
+    public void addProductToCatalog(Product product) {
+
+    }
+
+    public void removeProductFromCatalog(Product product) {
+
+    }
+
+    public void setProductAvailability(Product product) {
+
+    }
+
+    public void changeProductPrice(Product product) {
+
+    }
+
+    public void disbaleUser(User user) {
+
+    }
+
+    public void enableUser(User user) {
 
     }
 
