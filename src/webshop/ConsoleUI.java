adb699f2f546dad9d94109d85937df354a3e7bb3
ddeamonn <troo@inbox.lv>
2016-09-27 20:45:45 +0300

    Exceptions corrections

--- src/webshop/ConsoleUI.java
+++ src/webshop/ConsoleUI.java
@@ -30,40 +30,48 @@ public class ConsoleUI {
         boolean toContinue = true;
 
         while (toContinue) {
+            try {
+                ConsoleIO.showMessage("======== User Menu ========");
 
-            ConsoleIO.showMessage("======== User Menu ========");
+                ConsoleIO.showMessage("1 - User menu");
+                ConsoleIO.showMessage("2 - Product menu");
+                ConsoleIO.showMessage("3 - Sales menu");
+                ConsoleIO.showMessage("4 - Reports menu");
+                ConsoleIO.showMessage(Constants.EXIT + " - Exit");
 
-            ConsoleIO.showMessage("1 - User menu");
-            ConsoleIO.showMessage("2 - Product menu");
-            ConsoleIO.showMessage("3 - Sales menu");
-            ConsoleIO.showMessage("4 - Reports menu");
-            ConsoleIO.showMessage(Constants.EXIT + " - Exit");
-
-            ConsoleIO.showMessage("Please select: ");
-            int usersSelected = ConsoleIO.getUserInputInt();
-            switch (usersSelected) {
-                case 1:
-                    FormUser userForm = new FormUser(this.currentUser, userCommands);
-                    userForm.showFormUser();
-                    this.currentUser = userForm.getCurrentUser();
-                    this.setNewDbConnectionUser();
-                    break;
-                case 2:
-                    FormProducts formProducts = new FormProducts(this.currentUser, productsCommands);
-                    formProducts.showFormProducts();
-                    break;
-                case 3:
-                    FormSales formSales = new FormSales(this.currentUser, this.salesCommands,
-                            this.productsCommands);
-                    formSales.showFormBuySales();
-                    break;
-                case 4:
-                    FromReports formReports = new FromReports(this.currentUser, this.reportsCommands);
-                    formReports.showReportForm();
-                    break;
-                case Constants.EXIT:
-                    toContinue = false;
-                    break;
+                ConsoleIO.showMessage("Please select: ");
+                int usersSelected = ConsoleIO.getUserInputInt();
+                switch (usersSelected) {
+                    case 1:
+                        FormUser userForm = new FormUser(this.currentUser, userCommands);
+                        userForm.showFormUser();
+                        this.currentUser = userForm.getCurrentUser();
+                        this.setNewDbConnectionUser();
+                        break;
+                    case 2:
+                        FormProducts formProducts = new FormProducts(this.currentUser, productsCommands);
+                        formProducts.showFormProducts();
+                        break;
+                    case 3:
+                        FormSales formSales = new FormSales(this.currentUser, this.salesCommands,
+                                this.productsCommands);
+                        formSales.showFormBuySales();
+                        break;
+                    case 4:
+                        FromReports formReports = new FromReports(this.currentUser, this.reportsCommands);
+                        formReports.showReportForm();
+                        break;
+                    case Constants.EXIT:
+                        toContinue = false;
+                        break;
+                }
+            } catch (WebShopSqlException e) {
+                System.err.println(e.getClass().getName() + "Webshop exception: " + e.getMessage());
+            } catch (SecurityException e) {
+                System.err.println(e.getClass().getName() + "Security violation: " + e.getMessage());
+            } catch (Exception e) {
+                System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                System.exit(0);
             }
         }
     }
